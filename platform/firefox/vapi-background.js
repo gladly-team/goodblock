f4f70c997d7c29f7950a705d711b7609f4522fe4
Josiah Gaskin <josiahgaskin@gmail.com>
2015-07-29 23:53:27 -0700

    Fixed name

--- platform/firefox/vapi-background.js
+++ platform/firefox/vapi-background.js
@@ -44,7 +44,7 @@ vAPI.fennec = Services.appinfo.ID === '{aa3c5121-dab2-40e2-81ca-7ea25febc110}';
 /******************************************************************************/
 
 vAPI.app = {
-    name: 'uBlock',
+    name: 'Goodblock',
     version: location.hash.slice(1)
 };
 
